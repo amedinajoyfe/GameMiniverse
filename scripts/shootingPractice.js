434fab3ad11ce86fa51b7133fe92862d25f651a3
alejandromedinavtlab <alejandromedina@vt-lab.com>
2023-11-21 14:02:37 +0100

    Added shooting practice functionality
    To fix: missed shots counter

--- scripts/shootingPractice.js
+++ scripts/shootingPractice.js
@@ -1,12 +1,33 @@
+let points = 0;
+let missed = 0;
+
 $(document).ready(() =>  {
-    var cancel = setInterval(createCircle, 2000);
+    var cancel = setInterval(createCircle, 3000);
 });
 
 function createCircle(){
-    $(".target").remove();
+    console.log(missed);
     let top = Math.floor(Math.random() * 75) + 10;
     let left = Math.floor(Math.random() * 90);
     let size = Math.floor(Math.random() * 100) + 60;
-    // $("#gameZone").html('<div class="target" style="position: absolute; top: 50%; left: 30%;"></div>');
     $("#gameZone").append('<div class="target" style="width:' + size + 'px; height:' + size + 'px; position: absolute; top: ' + top + '%; left: ' + left + '%;"></div>');
+
+    var deleteCircle = setInterval(myCircle => {
+        missed += 1;
+        $("#timer").html("Missed: " + missed);
+        myCircle.remove();
+    }, 3000, $(".target:last-of-type"));
+
+    $(".target:last-of-type").on("click", clicked.bind($(".target:last-of-type"), deleteCircle) );
+}
+
+function clicked(stopMiss){
+    points += 1;
+    $("#points").html("Points: " + points);
+    clearInterval(stopMiss);
+    this.remove();
+}
+
+function deleteCircle(myCircleircle){
+    myCircle.remove();
 }
