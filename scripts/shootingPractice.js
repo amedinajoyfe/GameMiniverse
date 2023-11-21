8dd95a853305326667fed6ea6c6af076b9ee22a6
Alejandro Medina Galán <122613387+amedinajoyfe@users.noreply.github.com>
2023-11-21 18:16:36 +0100

    Added shooting practice functionality
    Missing the ending, also fixed positioning of game divs in index.html

--- scripts/shootingPractice.js
+++ scripts/shootingPractice.js
@@ -1,30 +1,38 @@
 let points = 0;
 let missed = 0;
+let speed = 3000;
 
 $(document).ready(() =>  {
-    var cancel = setInterval(createCircle, 3000);
+    var cancel = setTimeout(createCircle, speed);  //Mirar setTimeOut vs setInterval para las funciones
 });
 
 function createCircle(){
-    console.log(missed);
-    let top = Math.floor(Math.random() * 75) + 10;
-    let left = Math.floor(Math.random() * 90);
-    let size = Math.floor(Math.random() * 100) + 60;
-    $("#gameZone").append('<div class="target" style="width:' + size + 'px; height:' + size + 'px; position: absolute; top: ' + top + '%; left: ' + left + '%;"></div>');
+    let size = Math.floor(Math.random() * 120) + 50;
+    let top = Math.floor(Math.random() * 80);
+    let left = Math.floor(Math.random() * 80);
 
-    var deleteCircle = setInterval(myCircle => {
+    let newDiv = $('<div class="target" style="width:' + size + 'px; height:' + size + 'px; top: ' + top + '%; left: ' + left + '%;"></div>');
+    $("#gameZone").append(newDiv);
+
+    var deleteCircle = setTimeout(myCircle => {
         missed += 1;
         $("#timer").html("Missed: " + missed);
         myCircle.remove();
-    }, 3000, $(".target:last-of-type"));
+    }, 2000, newDiv);
 
     $(".target:last-of-type").on("click", clicked.bind($(".target:last-of-type"), deleteCircle) );
+    
+    if(speed > 1000)
+    {
+        speed -= 200;
+    }
+    setTimeout(createCircle, speed); // Look into changing animation time from js
 }
 
 function clicked(stopMiss){
     points += 1;
     $("#points").html("Points: " + points);
-    clearInterval(stopMiss);
+    clearTimeout(stopMiss);
     this.remove();
 }
 
