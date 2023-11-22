4677abd2f3778f7ad8b766a709c2e6cdb3f9d92d
Alejandro Medina Galán <122613387+amedinajoyfe@users.noreply.github.com>
2023-11-22 19:33:27 +0100

    Added finish box for games

--- scripts/shootingPractice.js
+++ scripts/shootingPractice.js
@@ -1,41 +1,52 @@
 let points = 0;
 let missed = 0;
-let speed = 3000;
+let speed = 2600;
+let limit = 600;
 
 $(document).ready(() =>  {
-    var cancel = setTimeout(createCircle, speed);  //Mirar setTimeOut vs setInterval para las funciones
+    setTimeout(createCircle, speed);  //Mirar setTimeOut vs setInterval para las funciones
 });
 
 function createCircle(){
     let size = Math.floor(Math.random() * 120) + 50;
     let top = Math.floor(Math.random() * 80);
     let left = Math.floor(Math.random() * 80);
-
     let newDiv = $('<div class="target" style="width:' + size + 'px; height:' + size + 'px; top: ' + top + '%; left: ' + left + '%;"></div>');
     $("#gameZone").append(newDiv);
 
     var deleteCircle = setTimeout(myCircle => {
         missed += 1;
-        $("#timer").html("Missed: " + missed);
+        if(missed <= 3)
+            $("#missed").html("Fallos: " + missed);
         myCircle.remove();
     }, 2000, newDiv);
 
     $(".target:last-of-type").on("click", clicked.bind($(".target:last-of-type"), deleteCircle) );
     
-    if(speed > 1000)
+    if(speed > limit)
     {
         speed -= 200;
+    }
+    if(missed > 2){
+        $(".target").remove();
+        return;
     }
     setTimeout(createCircle, speed); // Look into changing animation time from js
 }
 
 function clicked(stopMiss){
     points += 1;
-    $("#points").html("Points: " + points);
+    $("#points").html("Puntos: " + points);
     clearTimeout(stopMiss);
     this.remove();
 }
 
 function deleteCircle(myCircleircle){
     myCircle.remove();
+}
+
+function testLoss(stopGame){
+    if(missed > 3){
+        clearTimeout(stopGame);
+    }
 }
