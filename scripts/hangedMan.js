c6ae6a1ec8f25879d1c1df5c2d903474eafa0bba
Alejandro Medina Galán <122613387+amedinajoyfe@users.noreply.github.com>
2023-11-30 18:08:03 +0100

    Started HangedMan logic

--- scripts/hangedMan.js
+++ scripts/hangedMan.js
@@ -1,12 +1,35 @@
+let insertString = "";
+let currentState = "";
+let word = "PALABRA LOCO";
 $(document).ready(function(){
-    let word = "palabra y no";
-    let insertString = "";
+    for (let index = 0; index < word.length; index++) {
+        currentState += "_";
+    }
+    generateStringVisually(word);
+    $("#word").html(insertString);
+
+    $(".keyboardLetter").on("click", event => {
+        let letterClicked = event.target.innerText;
+        for (let index = 0; index < word.length; index++) {
+            if(word.charAt(index) == letterClicked){
+                currentState = currentState.setCharAt(currentState, index, 'g');
+                alert(currentState);
+            }
+        }
+    })
+})
+
+function generateStringVisually(word){
     for (let index = 0; index < word.length; index++) {
         if(word.charAt(index) == " "){
             insertString += "<div class='letter'><h1> </h1></div>";
             continue;
         }
-        insertString += "<div class='letter'><h1>_</h1></div>";
+        insertString += "<div class='letter'><h1>" + currentState.charAt(index) + "</h1></div>";
     }
-    $("#word").html(insertString);
-})
+}
+
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) return str;
+    return str.substring(0,index) + chr + str.substring(index+1);
+}
